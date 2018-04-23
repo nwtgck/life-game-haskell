883704d2da0b2f0590347c3ccb5294cb7207f0f5
Ryo Ota <nwtgck@gmail.com>
2018-04-23 17:53:18 +0900

    [change] Use hscurses to display field

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Concurrent
 import System.Environment
 import System.Process
+import qualified UI.HSCurses.Curses as Curses
 
 import LifeGame
 
@@ -14,10 +15,14 @@ main = do
     let (w, h) = (read wS :: Int, read hS :: Int)
     let size = (w, h)
     field <- randomField size
+    Curses.initCurses
     gameStart field size $ \field -> do
-      system "clear"
-      print field
+      Curses.erase
+      let !fieldStr = show field
+      Curses.wAddStr Curses.stdScr fieldStr
+      Curses.refresh
       threadDelay 30000
+    Curses.endWin
   else putStrLn "Specify size of the field"
 
   return ()
